40cf8a2c83ef1db22b76045c08341a3224417d64
hannya <hanny.aly@gmail.com>
2020-08-20 00:12:38 -0400

    Add control elements

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
+    state = {
+        term: ''
+    };
+
     render() {
         return (
-            <div>
-                <form>
-                    <input type="text" />
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image search</label>
+                        <input 
+                            type="text" 
+                            value={this.state.term}
+                            onChange={(e) => this.setState({term: e.target.value})}
+                        />
+                    </div>
                 </form>
             </div>
         );
