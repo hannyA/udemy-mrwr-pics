3c8b4fea8d0c7473583cd7f90002bcd7ef850147
hannya <hanny.aly@gmail.com>
2020-08-20 23:40:09 -0400

    Convert App to component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,18 +2,19 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
 
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
-
     state = {
         term: ''
+    };
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.term);
     };
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image search</label>
                         <input 
